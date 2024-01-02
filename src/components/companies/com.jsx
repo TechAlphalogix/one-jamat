e847f720bd6662e24ff8b7b6180e8a0f8da4507d
Ali Shan <optimist.alishan@gmail.com>
2024-01-02 22:46:06 +0500

    pushng

--- src/components/companies/com.jsx
+++ src/components/companies/com.jsx
@@ -3,6 +3,7 @@ import IMX from '../../assets/imx.png';
 import Alpha from '../../assets/alpha.png';
 import Pine from '../../assets/pine.png';
 import UCO from '../../assets/uco.png';
+import Tz from '../../assets/tz.png';
 
 export default function CompaniesList() {
     const companyData = [
@@ -22,27 +23,26 @@ export default function CompaniesList() {
         },
         {
             name: 'Techzoid',
-            description: 'IdeoMetriX, founded in 2020, has rapidly evolved into a dynamic design and tech firm with a global footprint. In its three-year....',
+            description: 'Our IT company is dedicated to helping businesses and organizations enhance their online presence and capabilities. We offer a....',
             domains: ['UX Design', 'Graphic Design', 'Digital Marketing', 'Software Developement'],
             link: 'https://techzoid.pk/',
             link1: 'https://drive.google.com/file/d/1K5vts4n1U5anICZG-ys7UIxnwzazVJRH/view',
         },
         {
-            name: 'Pine tech',
-            description: 'IdeoMetriX, founded in 2020, has rapidly evolved into a dynamic design and tech firm with a global footprint. In its three-year....',
+            name: 'PineTechnologies',
+            description: 'We are reliable source for top-quality software solutions and branding services. We are dedicated to delivering excellence in....',
             domains: ['UX Design', 'Graphic Design', 'Digital Marketing', 'Software Developement'],
-            link: 'https://uconnect.pk/',
+            link: 'https://www.pine-technologies.com/',
             link1: 'https://drive.google.com/file/d/1_JZiOFM3nkLl5eWF6ueJWgJFbRVF8GBD/view',
         },
-        // Add three more company objects with similar structure
-        // ...
         
     ];
 
     const companyLogos = {
         IdeoMatrix: IMX,
         Techalphalogix: Alpha,
-        Techzoid: Pine,
+        PineTechnologies: Pine,
+        Techzoid: Tz,
         // Add entries for other companies as needed
     };
 
@@ -57,25 +57,25 @@ export default function CompaniesList() {
                         {companyData.map((company, index) => (
                             <div
                                 key={index}
-                                className="w-full mx-auto bg-white shadow-2xl p-[20px] rounded-[4px]  flex flex-col md:flex-row my-1 sm:w-full  md:w-full md:my-1  lg:w-[47%] "
+                                className="w-full mx-auto bg-white shadow-2xl p-[20px] rounded-[4px]  flex flex-col md:flex-row my-1 sm:w-full  md:w-full md:my-1  lg:w-[49%] "
                             >
-                                <div className="logo flex justify-center items-center bg-slate-100 w-full md:w-2/5">
-                                    <Image src={companyLogos[company.name]} alt={company.name} />
+                                <div className="logo flex h-[150px] md:h-auto  justify-center items-center bg-slate-100 w-full md:w-2/5">
+                                    <Image className='hover:scale-110 transition-all' src={companyLogos[company.name]} width={136} alt={company.name} />
                                 </div>
-                                <div className="info ps-[10px] py-[20px] w-full md:w-3/5 ">
-                                <h3 className="font-semibold text-[24px] pt-[10px] leading-[28px]">{company.name}</h3>
+                                <div className="info ps-[10px]  w-full md:w-3/5 ">
+                                <h3 className="font-semibold text-[24px] pt-[5px] leading-[28px]">{company.name}</h3>
                                     <h4 className="font-normal text-[16px] pt-[10px]">{company.description}</h4>
                                     <h4 className="font-semibold text-[18px] leading-[26px] pt-[10px]">Domains</h4>
                                     <div className="dom flex flex-wrap flex-row">
                                         {company.domains.map((domain, index) => (
-                                            <p className='leading-[18.77px] font-normal text-[#2947A9] bg-[#eaedf6] px-[5px] mb-[10px] py-[10px] mt-[10px] rounded-[50px]  mx-[10px]' key={index}>{domain}</p>
+                                            <p className='leading-[18.77px] font-normal text-[#2947A9] bg-[#eaedf6] px-[5px] mb-[5px] py-[10px] mt-[5px] rounded-[50px]  me-[10px]' key={index}>{domain}</p>
                                         ))}
                                     </div>
-                                    <div className="btns flex flex-wrap py-[10px]">
-                                        <a target='_blank' rel='norefferer' className='text-[#2947A9] border-2 border-[#2947A9] px-[26px] rounded-md mx-[10px] py-[7px]' href={company.link}>
+                                    <div className="btns flex flex-wrap  space-x-1 py-[10px]">
+                                        <a target='_blank' rel='norefferer' className='text-[#2947A9] border-2 border-[#2947A9] px-[10px] md:px-[26px] rounded-md md:mx-[10px] py-[7px]' href={company.link}>
                                             Visit Website
                                         </a>
-                                        <a target='_blank' rel='norefferer' className='bg-[#2947A9] border-2 border-[#2947A9] text-white px-[26px] rounded-md mx-[10px] py-[7px]' href={company.link1}>
+                                        <a target='_blank' rel='norefferer' className='bg-[#2947A9] border-2 border-[#2947A9] text-white px-[10px] md:px-[26px] rounded-md md:mx-[10px] py-[7px]' href={company.link1}>
                                             View Profile
                                         </a>
                                     </div>
