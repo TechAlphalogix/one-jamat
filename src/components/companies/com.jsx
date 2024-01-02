348624ac059ae951793e98a6f4c2f02de14d9d9c
Ali Shan <optimist.alishan@gmail.com>
2024-01-02 21:49:49 +0500

    s

--- src/components/companies/com.jsx
+++ src/components/companies/com.jsx
@@ -11,7 +11,7 @@ export default function CompaniesList() {
             description: 'IdeoMetriX, founded in 2020, has rapidly evolved into a dynamic design and tech firm with a global footprint. In its three-year....',
             domains: ['UX Design', 'Graphic Design', 'Digital Marketing', 'Software Developement'],
             link: 'https://www.ideometrix.com/',
-            link1: 'sd',
+            link1: 'https://drive.google.com/file/d/1K5vts4n1U5anICZG-ys7UIxnwzazVJRH/view',
         },
         {
             name: 'Techalphalogix',
