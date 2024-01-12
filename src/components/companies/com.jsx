8e52cb3c416b475fb9df7faba73af51c89952e9e
Ali Shan <optimist.alishan@gmail.com>
2024-01-13 00:27:42 +0500

    pushing

--- src/components/companies/com.jsx
+++ src/components/companies/com.jsx
@@ -7,51 +7,70 @@ import Tz from "../../assets/tz.png";
 import DBI from "../../assets/dbi.png";
 import TSC from "../../assets/tscape.png";
 import CLD from "../../assets/cloudlem.png";
+import CALI from "../../assets/cali.png";
+import SASI from "../../assets/sassi.png";
 
 
 export default function CompaniesList() {
   const companyData = [
     {
-      name: "IdeoMatrix",
+      name: "Caliberon",
       description:
-        "IdeoMetriX, founded in 2020, has rapidly evolved into a dynamic design and tech firm with a global footprint. In its three-year....",
+        "CalibreOn International is a prominent figure in the Business Process Outsourcing (BPO) sector, on a mission....",
       domains: [
         "UX Design",
         "Graphic Design",
         "Digital Marketing",
         "Software Developement",
+      ],
+      link: "https://www.calibreon.com.pk/",
+      link1:
+        "https://google.com",
+    },
+    
+    {
+      name: "IdeoMatrix",
+      description:
+        "IdeoMetriX, founded in 2020, has rapidly evolved into a dynamic design and tech firm with a global footprint. In its three-year....",
+      domains: [
+        "Brand Auditing",
+        "Campaign Designing",
+        "Digital Marketing",
+        "UI/UX Designing",
       ],
       link: "https://www.ideometrix.com/",
       link1:
         "https://drive.google.com/open?id=1W1QDt29l_BekLcU4MzO48oCqB_6Ah-d5&authuser=0",
     },
-    {
-      name: "Techzoid",
-      description:
-        "Our IT company is dedicated to helping businesses and organizations enhance their online presence and capabilities. We offer a....",
-      domains: [
-        "UX Design",
-        "Graphic Design",
-        "Digital Marketing",
-        "Software Developement",
-      ],
-      link: "https://techzoid.pk/",
-      link1:
-        "https://drive.google.com/file/d/1K5vts4n1U5anICZG-ys7UIxnwzazVJRH/view",
-    },
+    
     {
       name: "Techalphalogix",
       description:
-        "IdeoMetriX, founded in 2020, has rapidly evolved into a dynamic design and tech firm with a global footprint. In its three-year....",
+        "Tech AlphaLogix Pvt. Ltd. is a technology-oriented company specializing in web development and design....",
       domains: [
-        "UX Design",
-        "Graphic Design",
-        "Digital Marketing",
-        "Software Developement",
+        "Web Application",
+        "UI/UX Design",
+        "SaaS Development",
+        "Social media marketing",
       ],
       link: "https://techalphalogix.com/",
       link1: "https://drive.google.com/file/d/1bUeWPsYo6Plsenoj05ql57hXb9e87bgL/view",
     },
+    {
+      name: "PineTechnologies",
+      description:
+        "We are reliable source for top-quality software solutions and branding services. We are dedicated to delivering excellence in....",
+      domains: [
+        "Web Development",
+        "Mobile app Development",
+        "CMS Development",
+        "UI/UX Design",
+      ],
+      link: "https://www.pine-technologies.com/",
+      link1:
+        "https://drive.google.com/file/d/1_JZiOFM3nkLl5eWF6ueJWgJFbRVF8GBD/view",
+    },
+    
     {
       name: "Uconnect",
       description:
@@ -67,28 +86,27 @@ export default function CompaniesList() {
         "https://drive.google.com/file/d/1Vuo92FOr6AOPeEtV2gfPzCdZe9pnhsuA/view",
     },
     {
-      name: "PineTechnologies",
+      name: "Techzoid",
       description:
-        "We are reliable source for top-quality software solutions and branding services. We are dedicated to delivering excellence in....",
+        "Our IT company is dedicated to helping businesses and organizations enhance their online presence and capabilities. We offer a....",
       domains: [
         "UX Design",
         "Graphic Design",
         "Digital Marketing",
-        "Software Developement",
+        "Software Solutions",
       ],
-      link: "https://www.pine-technologies.com/",
+      link: "https://techzoid.pk/",
       link1:
-        "https://drive.google.com/file/d/1_JZiOFM3nkLl5eWF6ueJWgJFbRVF8GBD/view",
+        "https://drive.google.com/file/d/1K5vts4n1U5anICZG-ys7UIxnwzazVJRH/view",
     },
     {
       name: "DesignBytesInternational",
       description:
         "Design Bytes International is a creative freelance company operating from Gilgit Baltistan, providing freelance services....",
       domains: [
-        "UX Design",
+        "Architectural Design",
         "Graphic Design",
-        "Digital Marketing",
-        "Software Developement",
+        "Accounts and Bookkeeping",
       ],
       link: "https://designbytesinternational.com/",
       link1:
@@ -99,10 +117,10 @@ export default function CompaniesList() {
       description:
         "Primarily TechScape is an ED-Tech company that operates with the vision to equip future generations for the future of....",
       domains: [
-        "UX Design",
-        "Graphic Design",
+        "Web Development",
+        "App Development",
         "Digital Marketing",
-        "Software Developement",
+        "UX/UI",
       ],
       link: "https://techscape.pk/",
       link1:
@@ -114,10 +132,23 @@ export default function CompaniesList() {
       description:
         "Cloudlem is an emerging DevOps consulting company with a wealth of experience in supporting customers globally to....",
       domains: [
-        "UX Design",
-        "Graphic Design",
-        "Digital Marketing",
-        "Software Developement",
+        "Cloud-computing",
+        "DevOps services",
+        "MERN Stack Development",
+        "Cloud server consultancy",
+      ],
+      link: "http://www.cloudlem.com",
+      link1:
+        "https://drive.google.com/file/d/1ZsrYNDvIWrhFXdIeHeMsQYs7CDRLspOc/view?usp=drive_open",
+    },
+    {
+      name: "SASI",
+      description:
+        "SASI is a professional freelance and Outsourced accounting, financial reporting and consulting services provider....",
+      domains: [
+        "Accounting",
+        "Bookkeeping",
+        "Tax Services",
       ],
       link: "http://www.cloudlem.com",
       link1:
@@ -134,6 +165,8 @@ export default function CompaniesList() {
     Techscape: TSC,
     Cloudlem: CLD,
     Uconnect: UCO,
+    Caliberon: CALI,
+    SASI: SASI,
   };
 
   return (
